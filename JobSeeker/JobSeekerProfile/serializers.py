6d3c89564c70712f7434e3a643b287856b297d34
KyawLynnAwng <kyawsoehla.developer@gmail.com>
2025-08-29 18:41:49 -0700

    fiexd jobseeker err

--- JobSeeker/JobSeekerProfile/serializers.py
+++ JobSeeker/JobSeekerProfile/serializers.py
@@ -3,6 +3,8 @@ from Accounts.models import *
 
 
 class JobSeekerSignInSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(required=True)
     class Meta:
         model = CustomUser
         fields = ["id", "email",]
--- JobSeeker/frontend/src/utils/constants/employerConstants.js
+++ JobSeeker/frontend/src/utils/constants/employerConstants.js
@@ -1,7 +1,7 @@
 export const EMPLOYER_API = {
-REGISTER: "/auth/employer/register/",
-  SIGNIN: "/auth/employer/signin/",
+REGISTER: "/accounts-employer/employer/preregister/",
+  SIGNIN: "/accounts-employer/employer/login/",
   PROFILE: "/auth/employer/profile/",
-  COMPANY_DETAIL: "/accounts-employer/employer/register/",
+  COMPANY_DETAIL: "/accounts-employer/employer/register/employer/",
   LOGOUT: "/auth/logout/",
 };
