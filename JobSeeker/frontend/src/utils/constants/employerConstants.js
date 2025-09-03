2c1041a74fc1de001d95a217542a7574981ad7a7
Aung Paing <aungpaing@example.com>
2025-09-03 12:37:38 +0630

    url

--- JobSeeker/frontend/src/utils/constants/employerConstants.js
+++ JobSeeker/frontend/src/utils/constants/employerConstants.js
@@ -1,15 +1,5 @@
 export const EMPLOYER_API = {
-<<<<<<< HEAD
-  REGISTER: "/accounts-employer/employer/preregister/",
-  REGISTER_DETAIL: "/accounts-employer/employer/register/employer/",
-};
-=======
-
   REGISTER: "/accounts-employer/employer/preregister/",
   REGISTER_DETAIL: "/accounts-employer/employer/register/employer/",
 };
 
-
-
-
->>>>>>> 6d3001dfd73c50bab07c885ce7093520c14172f4
