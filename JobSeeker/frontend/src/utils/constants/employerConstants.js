94e17e5e6693fa7746f5b57a77a637cbdfac3b1a
Aung Paing <aungpaing@example.com>
2025-09-03 10:21:38 +0630

    fiexd something

--- JobSeeker/frontend/src/utils/constants/employerConstants.js
+++ JobSeeker/frontend/src/utils/constants/employerConstants.js
@@ -1,13 +1,4 @@
 export const EMPLOYER_API = {
-<<<<<<< HEAD
   REGISTER: "/accounts-employer/employer/preregister/",
   REGISTER_DETAIL: "/accounts-employer/employer/register/employer/",
 };
-=======
-REGISTER: "/accounts-employer/employer/preregister/",
-  SIGNIN: "/accounts-employer/employer/login/",
-  PROFILE: "/auth/employer/profile/",
-  COMPANY_DETAIL: "/accounts-employer/employer/register/employer/",
-  LOGOUT: "/auth/logout/",
-};
->>>>>>> 3def19bf5d5e4c7934910fb143a8d31f5b548a03
