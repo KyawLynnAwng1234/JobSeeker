645e765f9c17bdd6d2eb2717c19a4b3584d2e448
Than Soe <136292647+thansoe-2000@users.noreply.github.com>
2025-09-12 09:03:43 +0630

    Thansoe (#39)
    * I changed category list view and create view
    
    * WIP: my gitignore changes
    
    * I delete duplicate code
    
    * I changed <int:pk> to <uuid:pk> and changed some code in jobs.views.py
    
    * I delete conflict code
    
    * conflict code
    
    * update views.py
    
    * fixed
    
    * to pull
    
    * changed some code in jobs/views.py
    
    * fixed git hub error

--- JobSeeker/Jobs/models.py
+++ JobSeeker/Jobs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db import models
 from EmployerProfile.models import EmployerProfile
+from Accounts.models import CustomUser
 import uuid
 
 
@@ -14,6 +15,7 @@ class JobCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
     updated_at = models.DateTimeField(auto_now=True,blank=True,null=True)
+    user=models.ForeignKey(CustomUser,on_delete=models.CASCADE,blank=True,null=True)
 
     def __str__(self):
         return self.name
