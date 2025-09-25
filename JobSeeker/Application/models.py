bd8243cb1ad5eb598320d9924118a5b571380e6d
KyawLynnAwng <kyawsoehla.developer@gmail.com>
2025-09-25 08:49:48 +0630

    add notification for application

--- JobSeeker/Application/models.py
+++ JobSeeker/Application/models.py
@@ -13,6 +13,16 @@ class SaveJob(models.Model):
     job = models.ForeignKey(Jobs, on_delete=models.CASCADE, null=True, blank=True)
 
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["profile", "job"],
+                name="unique_save_job_per_jobseeker_job"
+        )
+    ]
+
+
+
 class Application(models.Model):
     STATUS_CHOICES = [
         ('P', 'Pending'),
@@ -24,15 +34,25 @@ class Application(models.Model):
         default=uuid.uuid4,    # Auto-generate UUID v4
         editable=False         # User လက်နဲ့ မပြင်နိုင်အောင် lock
     )
-    job_seeker_profile = models.ForeignKey(JobseekerProfile, on_delete=models.CASCADE,null=True, blank=True)
-    job = models.ForeignKey(Jobs, on_delete=models.CASCADE, null=True, blank=True)
-    resume = models.ForeignKey(Resume, on_delete=models.SET_NULL, null=True, blank=True)
+    job_seeker_profile = models.ForeignKey(JobseekerProfile,on_delete=models.CASCADE,null=True)
+    job = models.ForeignKey(Jobs, on_delete=models.CASCADE, null=True)
+    resume = models.ForeignKey(Resume, on_delete=models.SET_NULL, null=True)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES,default='P',null=True, blank=True)
-    cover_letter_text = models.TextField(null=True, blank=True)
+    cover_letter_text = models.TextField(null=True)
     applied_at = models.DateTimeField(auto_now_add=True,null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True,null=True, blank=True)
 
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["job_seeker_profile", "job"],
+                name="unique_application_per_jobseeker_job"
+        )
+    ]
+
     def __str__(self):
-        return self.cover_letter_text
+        return f"{self.job_seeker_profile} → {self.job} ({self.status})"
+
 
 
